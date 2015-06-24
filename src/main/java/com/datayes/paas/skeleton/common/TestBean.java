4bdd7089ea5dc782a772cdfb15332bfd7badfabf
qianzhong.fu <qianzhong.fu@datayes.com>
2015-06-24 18:34:48 +0800

    定时任务以及服务器启动时执行程序

---
+++ src/main/java/com/datayes/paas/skeleton/common/TestBean.java
@@ -0,0 +1,28 @@
+package com.datayes.paas.skeleton.common;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ * Created by qianzhong.fu on 2015/6/24.
+ */
+@Component
+public class TestBean {
+    private static final long FIXED_RATE = 60*1000L;
+
+    @PostConstruct
+    public void sayHello(){
+        System.out.println("Hello World !!!");
+    }
+    @PreDestroy
+    public void sayGoodbye(){
+        System.out.println("Goodbye World !!!");
+    }
+    @Scheduled( fixedRate = FIXED_RATE)
+    public void sayByTime(){
+        System.out.println("Hi World !!!");
+    }
+}
